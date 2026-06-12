fec99f5a9a4ab2e8a4074dd179226faf6b965529
Sathvik <Sathvik.chadalavada@gmail.com>
2026-06-13 00:09:24 +0530

    Added Datatypes Program

---
+++ Basics/Datatypes.java
@@ -0,0 +1,30 @@
+package Basics;
+
+public class Datatypes {
+    public static void main(String[] args) {
+
+        byte b = 100;
+        short s = 1000;
+        int i = 100000;
+        long l = 1000000000L;
+
+        float f = 5.5f;
+        double d = 10.99;
+
+        char c = 'S';
+
+        boolean bool = true;
+
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("int: " + i);
+        System.out.println("long: " + l);
+
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+
+        System.out.println("char: " + c);
+
+        System.out.println("boolean: " + bool);
+    }
+}
