a4dc3e4469f775034328e3ca5bb984563cb91715
Sathvik <Sathvik.chadalavada@gmail.com>
2026-06-13 00:02:43 +0530

    Added Variables Program

--- Basics/Variables.java
+++ Basics/Variables.java
@@ -3,34 +3,14 @@ package Basics;
 public class Variables {
     public static void main(String[] args) {
 
-        // Integer types
-        byte age = 18;
-        short year = 2026;
-        int population = 1500000;
-        long distanceToMoon = 384400000L;
-
-        // Decimal types
-        float cgpa = 8.5f;
-        double pi = 3.14159265359;
-
-        // Character
-        char grade = 'A';
-
-        // Boolean
-        boolean isJavaFun = true;
-
-        // Non-primitive
         String name = "Sathvik";
+        int age = 18;
 
-        // Print Output
-        System.out.println("Name: " + name);
-        System.out.println("Age: " + age);
-        System.out.println("Year: " + year);
-        System.out.println("Population: " + population);
-        System.out.println("Distance to Moon: " + distanceToMoon);
-        System.out.println("CGPA: " + cgpa);
-        System.out.println("Pi: " + pi);
-        System.out.println("Grade: " + grade);
-        System.out.println("Is Java Fun? " + isJavaFun);
+        System.out.println(name);
+        System.out.println(age);
+
+        age = 19;
+
+        System.out.println("Updated Age: " + age);
     }
 }
