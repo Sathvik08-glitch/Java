889e376890f40d46275fbec8399aaf145285b481
Sathvik <Sathvik.chadalavada@gmail.com>
2026-06-14 00:19:57 +0530

    Added Polymorphism Program

---
+++ OOPS/Polymorphism.java
@@ -0,0 +1,39 @@
+package OOPS;
+
+class Animal {
+
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Cat extends Animal {
+
+    @Override
+    void sound() {
+        System.out.println("Cat Meows");
+    }
+}
+
+
+public class Polymorphism {
+    public static void main(String[] args) {
+
+        Animal a = new Animal();
+        a.sound();
+
+        Dog d = new Dog();
+        d.sound();
+
+        Cat c = new Cat();
+        c.sound();
+    }
+}
