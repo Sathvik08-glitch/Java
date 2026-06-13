b8e221c897a84ee79fe79f6a6639e09ba4718ac9
Sathvik <Sathvik.chadalavada@gmail.com>
2026-06-13 09:50:03 +0530

    Added InputOutput Program

---
+++ Basics/InputOutput.java
@@ -0,0 +1,18 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class InputOutput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+
+        System.out.println("Hello, I am " + name + "! I am " + age + " years old.");
+    }    
+}
