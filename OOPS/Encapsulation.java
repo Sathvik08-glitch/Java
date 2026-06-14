3f4a744fb45758bd4866b35b1dd793ab386492f2
Sathvik <Sathvik.chadalavada@gmail.com>
2026-06-14 19:32:53 +0530

    Added Encapsulation Program

---
+++ OOPS/Encapsulation.java
@@ -0,0 +1,36 @@
+package OOPS;
+
+class Student2 {
+
+    private String name;
+    private int age;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
+public class Encapsulation {
+    public static void main(String[] args) {
+
+        Student2 s1 = new Student2();
+
+        s1.setName("Sathvik");
+        s1.setAge(20);
+
+        System.out.println("Name: " + s1.getName());
+        System.out.println("Age: " + s1.getAge());
+    }
+}
