2901118b07204953fd3a837ac7392d2ff91c0fd9
Sathvik <Sathvik.chadalavada@gmail.com>
2026-06-16 20:59:21 +0530

    Added MultipleCatch Program

---
+++ ExceptionHandling/MultipleCatchExample.java
@@ -0,0 +1,25 @@
+package ExceptionHandling;
+
+public class MultipleCatchExample {
+    public static void main(String[] args) {
+        
+        try {
+            
+            int[] numbers = {10,20,30,40,50};
+
+            System.out.println(numbers[5]);
+
+        } catch (ArithmeticException e) {
+
+            System.out.println("Arithmetic Error!");
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+
+            System.out.println("Array Index Error!");
+
+
+        }
+
+        System.out.println("Program Continues...");
+    }
+}
