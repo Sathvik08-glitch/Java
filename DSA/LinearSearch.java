7335b17cc1d369a4cdaa343f070d0321e294c8e1
Sathvik <Sathvik.chadalavada@gmail.com>
2026-06-29 19:13:20 +0530

    Added LinearSearch Program

--- DSA/LinearSearch.java
+++ DSA/LinearSearch.java
@@ -3,12 +3,18 @@ package DSA;
 public class LinearSearch {
     public static void main(String []args) {
         int [] numbers = {12, 45, 7, 89, 34};
-        int target = 89;
-
+        int target = 13;
+        boolean found = false;
         for(int i = 0; i<numbers.length; i++ ){
-            if(numbers[i] == target){
+                if(numbers[i] == target){
+                    found = true;
                 System.out.println("Found the required value at: " + i);
+                
             }
+
         }
+        if (!found){
+                    System.out.println("Element not found");
+                }
     }
 }
